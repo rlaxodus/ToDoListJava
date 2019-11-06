c2563d50a6224626b835777b61792405de9a7e50
P1326572 <P1326572@NCS-210819KK05.ncs.corp.int-ads>
2019-11-06 10:28:58 +0800

    Initial Commit

---
+++ src/ToDoList.java
@@ -0,0 +1,42 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class ToDoList {
+    private final HashMap<String, Task> tasks = new HashMap<String, Task>();
+
+    public void addTask(final Task task) {
+        // Add code here
+        this.tasks.put(task.getDescription(), task);
+    }
+
+    public void completeTask(final String description) {
+        // Add code here
+        this.tasks.get(description).setComplete(true);
+    }
+
+    public boolean getStatus(final String description) {
+        // Add code here
+        return this.tasks.get(description).isComplete();
+    }
+
+    public Task getTask(final String description) {
+        // Add code here
+        return this.tasks.get(description);
+    }
+
+    public Task removeTask(final String description) {
+        // Add code here
+        return this.tasks.remove(description);
+    }
+
+    public Collection<Task> getAllTasks() {
+        return this.tasks.values();
+    }
+
+    public Collection<Task> getCompletedTasks() {
+        // Add code here
+        return this.tasks.values().stream().filter(task -> task.isComplete() == true).collect(Collectors.toList());
+
+    }
+}
---
+++ test/ToDoListTest.java
@@ -0,0 +1,78 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ToDoListTest extends TestCase {
+    // Define Test Fixtures
+
+    public ToDoListTest() {
+        super();
+    }
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        // Initialise Test Fixtures
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        // Uninitialise test Fixtures
+    }
+
+    @Test
+    public void testAddTask() {
+        // Given
+        final ToDoList toDoList = new ToDoList();
+        final Task task = new Task("Task 1", false);
+
+        // When
+        toDoList.addTask(task);
+
+        // Then
+        assertEquals(toDoList.getAllTasks().size(), 1);
+    }
+
+    @Test
+    public void testgetStatus() {
+
+        // Given
+        final ToDoList toDoList = new ToDoList();
+        final Task task = new Task("Task 2", false);
+        toDoList.addTask(task);
+
+        // Then
+        assertFalse(toDoList.getStatus("Task 2"));
+    }
+
+    @Test
+    public void testRemoveTask() {
+        // Given
+        final ToDoList toDoList = new ToDoList();
+        final Task task = new Task("Task 3", false);
+        toDoList.addTask(task);
+        assertEquals(toDoList.getAllTasks().size(), 1);
+
+        // When
+        assertEquals(task, toDoList.removeTask("Task 3"));
+
+        // Then
+        assertEquals(toDoList.getAllTasks().size(), 0);
+    }
+
+    @Test
+    public void testGetCompletedTasks() {
+        // Given
+        final ToDoList toDoList = new ToDoList();
+        final Task task = new Task("Task 4", false);
+        toDoList.addTask(task);
+        assertEquals(toDoList.getAllTasks().size(), 1);
+        toDoList.completeTask("Task 4");
+
+        // Then
+        assertEquals(toDoList.getCompletedTasks().size(), 1);
+    }
+}
